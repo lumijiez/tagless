fd684d29d08d31b72369699627d50eeb837c8bf6
Daniel <danthevip@gmail.com>
2024-07-24 21:39:14 +0300

    fixed crashes

--- src/main/java/com/lumijiez/tagless/Tagless.java
+++ src/main/java/com/lumijiez/tagless/Tagless.java
@@ -2,20 +2,31 @@ package com.lumijiez.tagless;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Tagless.MODID, name = Tagless.NAME, version = Tagless.VERSION)
 public class Tagless {
     public static final String MODID = "tagless";
     public static final String NAME = "Tagless";
     public static final String VERSION = "1.0";
+
+    public static final Logger logger = LogManager.getLogger();
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
